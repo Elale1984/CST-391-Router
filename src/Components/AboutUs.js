9eed38c23d2bf7dd756db8688762d172cc9b7e78
Edward Lale <edward.gratefuldead1@gmail.com>
2023-05-21 21:03:35 -0500

    init

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutUs = () => {
+    return (<div>
+        <h1>About Your Dev Solution</h1>
+        <p>Need a website or mobile app developed for your company? Have no fear.... Your Dev Solution is here!</p>
+    </div>)
+}
+
+export default AboutUs;
---
+++ src/Components/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return(<div>
+        <h2>Your Dev Solution</h2>
+        <p>123 Developer Ln</p>
+        <p>Hutchinson, KS</p>
+        <p>(515)332-2255</p>
+    </div>);
+}
+
+export default ContactUs;
